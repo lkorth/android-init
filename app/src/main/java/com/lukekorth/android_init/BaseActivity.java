952d687c044910c7e9560b9ea7daf9dcc684019f
lkorth <luke@lukekorth.com>
2016-09-16 09:55:46 -0400

    Initial commit

---
+++ app/src/main/java/com/lukekorth/android_init/BaseActivity.java
@@ -0,0 +1,46 @@
+package com.lukekorth.android_init;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class BaseActivity extends AppCompatActivity {
+
+    protected Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(getLayout());
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+    protected int getLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/lukekorth/android_init/BaseApplication.java
@@ -0,0 +1,6 @@
+package com.lukekorth.android_init;
+
+import android.app.Application;
+
+public class BaseApplication extends Application {
+}
