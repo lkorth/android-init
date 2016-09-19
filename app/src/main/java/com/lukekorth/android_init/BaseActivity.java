85934aecfa1109c357a6b0e82f26c72fc4ff13ef
lkorth <luke@lukekorth.com>
2016-09-18 22:42:53 -0400

    Remove menu from BaseActivity

--- app/src/main/java/com/lukekorth/android_init/BaseActivity.java
+++ app/src/main/java/com/lukekorth/android_init/BaseActivity.java
@@ -3,8 +3,6 @@ package com.lukekorth.android_init;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.lukekorth.android_init.api.Api;
@@ -30,26 +28,5 @@ public class BaseActivity extends AppCompatActivity {
 
     protected int getLayout() {
         return R.layout.activity_main;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
